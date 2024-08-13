f29acd6f9ae0197d4abba87b6f7001e311eca268
agrimes23 <107214142+agrimes23@users.noreply.github.com>
2024-08-13 15:53:45 -0500

    basic layout for projects page

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -2,7 +2,25 @@ import React from 'react'
 
 const Projects = () => {
   return (
-    <div>Projects</div>
+    <div className="flex flex-col min-h-screen items-center">
+
+      <div className="my-10">
+        <h2 className="text-[3rem] text-accent">Projects</h2>
+      </div>
+
+      {/* Project list container */}
+      <div>
+        <div className="bg-neutralSecondary border-2 border-neutralSecondary-dark rounded-lg py-20 my-32 w-[60vw] flex items-center">
+
+          <p>project title</p>
+          <p>date last worked on</p>
+          <p>description</p>
+          <p>languages used (bar?)</p>
+          <p>title</p>
+        </div>
+      </div>
+      
+    </div>
   )
 }
 
