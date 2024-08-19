428abc7464fa30cfafb4c5ec44cea0d1fdea151e
agrimes23 <107214142+agrimes23@users.noreply.github.com>
2024-08-19 14:36:36 -0500

    using vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/react"
 import { Inter } from "next/font/google";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer"
@@ -22,6 +23,7 @@ export default function RootLayout({
         <Navbar />
         {children}
         <Footer />
+        <Analytics/>
       </body>
     </html>
   );
